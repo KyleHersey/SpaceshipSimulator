49f4113279406b6837373f558cbae593507e2f3a
KyleHersey <herseykyle@yahoo.com>
2016-03-08 16:08:35 -0600

    Added reset button (nonfunctional)

--- src/TheApplet.java
+++ src/TheApplet.java
@@ -1,6 +1,11 @@
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JApplet;
 
 public class TheApplet extends JApplet{
+	Model m;
 
 	public TheApplet(){}
 	
@@ -13,18 +18,28 @@ public class TheApplet extends JApplet{
 		Window win = new Window();
 		win.setSize(windowSizeX, windowSizeY);
 		this.setSize(windowSizeX, windowSizeY);
-		this.setVisible(true);
+
 
 		getContentPane().add(win);
 		
 		//make the model
-		Model m = new Model(win);
-		
+		m = new Model(win);
 		//bind the model to the Panel and display it
 		win.setModel(m);
 		
+		//add reset and pause button
+		Button reset = new Button("Reset");
+		reset.addActionListener(new resetAction(this));
+		reset.setVisible(true);
+		win.add(reset);
+		
+		this.validate();
+		
+		this.setVisible(true);
+		
 		//this ought to be in m.start(), which would return when the game ends
 		while(!m.gameOver){
+			System.out.println("ticking");
 			m.tick();
 			try {
 				Thread.sleep(15);
@@ -33,9 +48,22 @@ public class TheApplet extends JApplet{
 				e.printStackTrace();
 			}	//approximately 30fps
 			win.repaint();
-			this.repaint();
 		}	
 		System.out.println("Game Over");
+	}
+	
+	public class resetAction implements ActionListener{
+		TheApplet j;
+		
+		public resetAction(TheApplet j){
+			this.j = j;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			j.m.gameOver = true;
+			j.init();
+		}
 	}
 	
 	
